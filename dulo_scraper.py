467f100ea60fef8dcdba48537efc0ac18acad006
Dinn <zddin73@gmail.com>
2026-06-10 21:09:29 +0800

    Update dulo_scraper.py

--- dulo_scraper.py
+++ dulo_scraper.py
@@ -31,8 +31,7 @@ EPG_API      = "https://epg.pw/api/epg.xml?channel_id={channel_id}"
 MAX_WORKERS  = 10  # Number of concurrent EPG downloads
 
 # Stream headers required to prevent 403 Forbidden errors
-STREAM_USER_AGENT = "otg/1.5.1 (AppleTv Apple TV 4; tvOS16.0; appletv.client) libcurl/7.58.0 OpenSSL/1.0.2o zlib/1.2.11 clib/1.8.56
-"
+STREAM_USER_AGENT = "otg/1.5.1 (AppleTv Apple TV 4; tvOS16.0; appletv.client) libcurl/7.58.0 OpenSSL/1.0.2o zlib/1.2.11 clib/1.8.56"
 STREAM_REFERER    = "https://dulo.tv/"
 
 EPG_HEADERS = {
